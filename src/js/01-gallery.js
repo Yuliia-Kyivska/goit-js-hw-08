3db0c5605e1a73b79166e28d2c289f9e3c39c1b4
Yuliia-Kyivska <kiev.lia@gmail.com>
2022-11-06 19:37:37 +0200

    change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,5 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
@@ -9,12 +7,11 @@ import { galleryItems } from './gallery-items';
 let pictures = document.querySelector(".gallery");
 let picturesMarkup = createPicturesMarkup(galleryItems);
 pictures.insertAdjacentHTML("beforeend", picturesMarkup);
-pictures.addEventListener('click', onClickPicture);
+
 
 function createPicturesMarkup(galleryItems){
   return galleryItems.map(({ preview, original, description }) => {
       return `
-    <div class="gallery__item">
       <a
         class="gallery__link"
         href = '${original}'
@@ -23,52 +20,16 @@ function createPicturesMarkup(galleryItems){
           class="gallery__image"
           src="${preview}"
           data-source="${original}"
-          alt="${description}"
+          alt="Image ${description}"
+          title="beautiful picture"
         />
       </a>
-   </div>
     `;
     })
     .join("");
 }
 
-function onClickPicture(event) {
-  event.preventDefault();
-  if (!event.target.classList.contains('gallery__image')) {
-    return;
-  }
-  let src = event.target.getAttribute('data-source');
-  instance.show(() => { 
-    instance.element().querySelector('img').src = src;
-  });
-  
-}
-let gallery = new SimpleLightbox('.gallery a');
-gallery.on('show.simplelightbox', function () {
-
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250
 });
 
-
-
-// const instance = basicLightbox.create(`
-//     <div class="modal" id="modal_id">
-//        <img
-//           class="gallery__image"
-//           src=""
-//           alt=""
-//         />
-//         <a>Close</a>
-//     </div>
-// `, {
-//     onShow: (instance) => {
-//         instance.element().querySelector('.modal').onclick = instance.close
-//         document.addEventListener('keydown', modalCloseByKey); 
-//   },
-//   onClose: (instance) => {
-//         document.removeEventListener('keydown', modalCloseByKey); 
-//   }
-// })
-
-// function modalCloseByKey(e) {
-// if (e.key === 'Escape') instance.close();
-// }
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+function timePlayer() { 
+
+}
+const onPlay = function(data) {
+    // data — це об’єкт, що містить властивості, характерні для цієї події
+};
+
+player.on('play', onPlay);
+
+
+ player.setCurrentTime(1000).then(function(seconds) {
+    // секунд = фактичний час, який гравець прагнув
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // час був меншим за 0 або більшим за тривалість відео
+            break;
+
+        default:
+            // сталася інша помилка
+            break;
+    }
+});   
