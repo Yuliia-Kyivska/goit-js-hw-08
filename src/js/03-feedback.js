e68ac0c7aa18d9d265c84448b85a45d04641a457
Yuliia-Kyivska <kiev.lia@gmail.com>
2022-11-06 23:32:47 +0200

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,20 +14,13 @@ const dataChanges = () => {
 localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 };
 
-
-form.addEventListener('submit', () => { 
-  
-});
-
-inputEmail.addEventListener('input', dataChanges);
-inputMessage.addEventListener('input', dataChanges);
+inputEmail.addEventListener('input', throttle(dataChanges, 500));
+inputMessage.addEventListener('input', throttle(dataChanges, 500));
 
 
 const onLoad = e => {
   let savedSettings = localStorage.getItem(STORAGE_KEY);
- 
 
-  
   if (savedSettings !== null) {
     const parsedSettings = JSON.parse(savedSettings);
     inputEmail.value = parsedSettings.email;
@@ -38,8 +31,20 @@ const onLoad = e => {
 
 window.addEventListener("load", onLoad);
 
+form.addEventListener('submit', onSubmit);
 
-// player.on('input', throttle(onTimeUpdate, 500));
-// function onTimeUpdate(data) {
-//   localStorage.setItem(STORAGE_KEY, data.seconds);
-// }
+function onSubmit(e) {
+  e.preventDefault();
+
+  let savedSettings = localStorage.getItem(STORAGE_KEY);
+  
+  if (savedSettings !== null) {
+    const parsedData = JSON.parse(savedSettings);
+
+    console.log(parsedData);
+    localStorage.removeItem(STORAGE_KEY);
+  }
+
+  form.reset();
+}
+
