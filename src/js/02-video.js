fe2e1459e449c4415a12d72e23ca5f07ab1fc43c
Yuliia-Kyivska <kiev.lia@gmail.com>
2022-11-06 19:59:19 +0200

    change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,26 +6,30 @@ const STORAGE_KEY = "videoplayer-current-time";
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-function timePlayer() { 
-
-}
-const onPlay = function(data) {
-    // data — це об’єкт, що містить властивості, характерні для цієї події
-};
-
-player.on('play', onPlay);
 
 
- player.setCurrentTime(1000).then(function(seconds) {
-    // секунд = фактичний час, який гравець прагнув
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // час був меншим за 0 або більшим за тривалість відео
-            break;
 
-        default:
-            // сталася інша помилка
-            break;
-    }
-});   
+
+// function timePlayer() { 
+
+// }
+// const onPlay = function(data) {
+//     // data — це об’єкт, що містить властивості, характерні для цієї події
+// };
+
+// player.on('play', onPlay);
+
+
+//  player.setCurrentTime(1000).then(function(seconds) {
+//     // секунд = фактичний час, який гравець прагнув
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // час був меншим за 0 або більшим за тривалість відео
+//             break;
+
+//         default:
+//             // сталася інша помилка
+//             break;
+//     }
+// });   
